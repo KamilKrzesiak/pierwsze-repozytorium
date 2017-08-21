0814ef0d3458f624a93f26756e02ecabcf8df5d6
kamil.krzesiak <kamil.krzesiak01@gmail.com>
2017-08-21 18:07:12 +0200

    poprawa

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,31 +11,31 @@ module.exports = function(grunt) {
           'style.css': 'style.sass'
         }
       }
+    },
+    imagemin: {
+      dynamic: {
+          files: [{
+              expand: true,
+              cwd: 'images/',
+              src: ['**/*.{png,jpg,gif}'],
+              dest: 'images/build/'
+          }]
+      }
+    },
+    watch: {
+      scripts: {
+          files: ['sass/*.sass'],
+          tasks: ['sass'],
+          options: {
+            spawn: false
+          }
+      }
     }
-  },
-  imagemin: {
-    dynamic: {
-        files: [{
-            expand: true,
-            cwd: 'DSC_0038',
-            src: ['**/*.{png,jpg,gif}'],
-            dest: 'images/build/'
-        }]
-    }
-  },
-  watch: {
-    scripts: {
-        files: ['sass/*.sass'],
-        tasks: ['sass'],
-        options: {
-            spawn: false,
-        },
-    }
-  }
-  );
+  });
   // Load the plugins tasks
   grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-imagemin')
-  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   // Default task(s).
   grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+};
